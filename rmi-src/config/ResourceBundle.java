13f786a5b8aa4852fe98b9a1aa748755a8643b92
lpenaud <l.penaud@zaclys.net>
2020-04-01 20:39:34 +0200

    Add rmi-src\config\ResourceBundle.java

---
+++ rmi-src/config/ResourceBundle.java
@@ -0,0 +1,32 @@
+package config;
+
+import java.util.MissingResourceException;
+
+class ResourceBundle {
+	
+	public ResourceBundle(String baseName) {
+		try {
+			this.rb = java.util.ResourceBundle.getBundle(baseName);			
+		} catch (MissingResourceException e) {
+			this.rb = null;
+		}
+	}
+	
+	public String getString(String key, String defaultValue) {
+		try {
+			return rb.getString(key);
+		} catch (MissingResourceException | NullPointerException e) {
+			return defaultValue;
+		}
+	}
+	
+	public Integer getInteger(String key, Integer defaultValue) {
+		try {
+			return Integer.valueOf(rb.getString(key));
+		} catch (MissingResourceException | NullPointerException e) {
+			return defaultValue;
+		}
+	}
+	
+	protected java.util.ResourceBundle rb;
+}
